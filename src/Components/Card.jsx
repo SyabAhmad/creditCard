e3b7c9e4b6433d23da275f6f9ab877ef1729a760
SyabAhmad <syabblogger@gmail.com>
2023-07-30 16:48:36 +0500

    changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/card.css";
 import { Globe, GalleryVertical } from "lucide-react";
 const Card = () => {
+  const [name, setName] = useState("Syed Syab Ahmad");
+  const [number, setNumber] = useState("1234 4321 4567 7543");
+  const [month, setMonth] = useState("05");
+  const [year, setYear] = useState("23");
   return (
     <>
       <div className="cardContainer">
@@ -16,21 +20,28 @@ const Card = () => {
             </div>
           </div>
           <div className="number">
-            <h1>1234 4311 2344 2311</h1>
+            <h1>{number}</h1>
           </div>
           <div className="lowerArea">
-            <div>Syed Syab Ahmad</div>
-            <div>05/15</div>
+            <div>{name}</div>
+            <div>
+              {month}
+              {"/"}
+              {year}
+            </div>
           </div>
         </div>
       </div>
       <div className="dataEntring">
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
+        <input type="text" maxLength={16} id="name" value={name} />
         <label htmlFor="number">Number</label>
-        <input type="text" id="number" />
-        <label htmlFor="expiryDate">Expiry Date</label>
-        <input type="text" id="expiryDate" />
+        <input type="text" maxLength={10} id="number" value={number} />
+        <div>
+          <label htmlFor="expiryDate">Expiry Date</label>
+          <input type="text" maxLength={2} id="expiryDate" value={month} />
+          <input type="text" maxLength={2} id="expiryDate" value={year} />
+        </div>
       </div>
     </>
   );
